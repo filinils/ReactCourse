c4e1200992999bd80b392a4cd788d6bb2bb9a58d
Piotr Budziszewski <*>
2018-08-29 09:32:28 +0200

    Mer data från servern

--- server/controllers/rooms.js
+++ server/controllers/rooms.js
@@ -52,7 +52,12 @@ var rooms = () => {
                 img: 'https://images.pexels.com/photos/849835/pexels-photo-849835.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
                 price: 200,
                 title: 'Samans coola bil',
-                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam pharetra enim dui, id laoreet arcu efficitur at. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nunc mollis, erat non tristique sagittis, nulla lectus placerat ex, eu sagittis dolor enim eget massa. Mauris ac feugiat turpis, eget ornare est. Praesent porta a dui dapibus malesuada. Duis rhoncus ante id iaculis consectetur. Morbi commodo nulla aliquam hendrerit hendrerit. Mauris facilisis mollis eros, quis elementum lorem faucibus at. Phasellus eget dolor ac nisl pellentesque suscipit nec aliquet lectus'
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam pharetra enim dui, id laoreet arcu efficitur at. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nunc mollis, erat non tristique sagittis, nulla lectus placerat ex, eu sagittis dolor enim eget massa. Mauris ac feugiat turpis, eget ornare est. Praesent porta a dui dapibus malesuada. Duis rhoncus ante id iaculis consectetur. Morbi commodo nulla aliquam hendrerit hendrerit. Mauris facilisis mollis eros, quis elementum lorem faucibus at. Phasellus eget dolor ac nisl pellentesque suscipit nec aliquet lectus',
+                type: 'Lägenhet',
+                guests: 10,
+                nrOfRooms: 2,
+                nrOfBeds: 3,
+                nrOfBathrooms: 1
             };
         } else if (id == 2) {
             mockData = {
@@ -60,7 +65,12 @@ var rooms = () => {
                 img: 'https://images.pexels.com/photos/91223/pexels-photo-91223.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
                 price: 9999,
                 title: 'A Jonas Ikea projekt',
-                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam pharetra enim dui, id laoreet arcu efficitur at. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nunc mollis, erat non tristique sagittis, nulla lectus placerat ex, eu sagittis dolor enim eget massa. Mauris ac feugiat turpis, eget ornare est. Praesent porta a dui dapibus malesuada. Duis rhoncus ante id iaculis consectetur. Morbi commodo nulla aliquam hendrerit hendrerit. Mauris facilisis mollis eros, quis elementum lorem faucibus at. Phasellus eget dolor ac nisl pellentesque suscipit nec aliquet lectus'
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam pharetra enim dui, id laoreet arcu efficitur at. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nunc mollis, erat non tristique sagittis, nulla lectus placerat ex, eu sagittis dolor enim eget massa. Mauris ac feugiat turpis, eget ornare est. Praesent porta a dui dapibus malesuada. Duis rhoncus ante id iaculis consectetur. Morbi commodo nulla aliquam hendrerit hendrerit. Mauris facilisis mollis eros, quis elementum lorem faucibus at. Phasellus eget dolor ac nisl pellentesque suscipit nec aliquet lectus',
+                type: 'Rum',
+                guests: 50,
+                nrOfRooms: 1,
+                nrOfBeds: 5,
+                nrOfBathrooms: 0
             };
         } else if (id == 3) {
             mockData = {
@@ -68,7 +78,12 @@ var rooms = () => {
                 img: 'https://images.pexels.com/photos/937482/pexels-photo-937482.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
                 price: 5,
                 title: 'B Trevlig family vac',
-                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam pharetra enim dui, id laoreet arcu efficitur at. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nunc mollis, erat non tristique sagittis, nulla lectus placerat ex, eu sagittis dolor enim eget massa. Mauris ac feugiat turpis, eget ornare est. Praesent porta a dui dapibus malesuada. Duis rhoncus ante id iaculis consectetur. Morbi commodo nulla aliquam hendrerit hendrerit. Mauris facilisis mollis eros, quis elementum lorem faucibus at. Phasellus eget dolor ac nisl pellentesque suscipit nec aliquet lectus'
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam pharetra enim dui, id laoreet arcu efficitur at. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nunc mollis, erat non tristique sagittis, nulla lectus placerat ex, eu sagittis dolor enim eget massa. Mauris ac feugiat turpis, eget ornare est. Praesent porta a dui dapibus malesuada. Duis rhoncus ante id iaculis consectetur. Morbi commodo nulla aliquam hendrerit hendrerit. Mauris facilisis mollis eros, quis elementum lorem faucibus at. Phasellus eget dolor ac nisl pellentesque suscipit nec aliquet lectus',
+                type: 'Hus',
+                guests: 35,
+                nrOfRooms: 5,
+                nrOfBeds: 10,
+                nrOfBathrooms: 5
             };
         } else {
             mockData = {
@@ -76,7 +91,12 @@ var rooms = () => {
                 img: 'https://images.pexels.com/photos/1362478/pexels-photo-1362478.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
                 price: 12944,
                 title: 'D rummet',
-                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam pharetra enim dui, id laoreet arcu efficitur at. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nunc mollis, erat non tristique sagittis, nulla lectus placerat ex, eu sagittis dolor enim eget massa. Mauris ac feugiat turpis, eget ornare est. Praesent porta a dui dapibus malesuada. Duis rhoncus ante id iaculis consectetur. Morbi commodo nulla aliquam hendrerit hendrerit. Mauris facilisis mollis eros, quis elementum lorem faucibus at. Phasellus eget dolor ac nisl pellentesque suscipit nec aliquet lectus'
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam pharetra enim dui, id laoreet arcu efficitur at. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nunc mollis, erat non tristique sagittis, nulla lectus placerat ex, eu sagittis dolor enim eget massa. Mauris ac feugiat turpis, eget ornare est. Praesent porta a dui dapibus malesuada. Duis rhoncus ante id iaculis consectetur. Morbi commodo nulla aliquam hendrerit hendrerit. Mauris facilisis mollis eros, quis elementum lorem faucibus at. Phasellus eget dolor ac nisl pellentesque suscipit nec aliquet lectus',
+                type: 'Rum',
+                guests: 2,
+                nrOfRooms: 1,
+                nrOfBeds: 1,
+                nrOfBathrooms: 0
             };
         }
 
