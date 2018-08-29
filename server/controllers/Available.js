6df3d5abbd155c122d308975fd89d23f08ef867f
Saman Muradi <saman.muradi@capgemini.com>
2018-08-29 10:47:52 +0200

    a
    a

--- server/controllers/Available.js
+++ server/controllers/Available.js
@@ -1,24 +1,24 @@
 var available = () => {
 
     function read(req, res) {
-           var dates = {
-               "5": {}, 
-               "4": {},
-               "6": {}
+        var dates = {
+            "5": {},
+            "4": {},
+            "6": {}
 
-           };
+        };
 
-            res.send(dates);
+        res.send(dates);
     }
-    
-	function create(req, res) {
-		
-	}
 
-   return {
-       read,
-       create
-   };
+    function create(req, res) {
+
+    }
+
+    return {
+        read,
+        create
+    };
 
 }
 
